2e4707a3131ad8645866d5d1a476e8c42cfc260c
Duy Olli <duy@olli-ai.com>
2018-08-08 13:46:30 +0700

    added gg similar

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ instance.use(bodyParser.urlencoded({ extended: false }));
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, instance);
-
-    app.listen(9000, process.env.NODE_ENV === 'prod' ? '0.0.0.0' : '');
+    app.listen(9000, '0.0.0.0');
 
     console.log("Application is listening on port 9000");
 }
